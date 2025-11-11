dfd0b063db21983b5c2e9f2f55d4e06fb627d526
Rintaru <shintaruruiz@gmail.com>
2025-11-11 22:42:54 +0800

    wip

--- cmd/Articy-Renpy-Exporter/main.go
+++ cmd/Articy-Renpy-Exporter/main.go
@@ -6,7 +6,7 @@ import (
 	"os"
 )
 
-type Manifest struct {
+type _Manifest_json struct {
 	Packages []struct {
 		Name  string `json:"Name"`
 		Files struct {
@@ -17,19 +17,11 @@ type Manifest struct {
 	} `json:"Packages"`
 }
 
-// type Package_Objects struct {
-// 	Objects []struct {
-// 		Type       string `json:"Entity"`
-// 		Properties struct {
-// 		}
-// 	}
-// }
-
-type Heirarchy struct {
-	Id            string       `json:"Id"`
-	TechnicalName string       `json:"TechnicalName"`
-	Type          string       `json:"Type"`
-	Children      *[]Heirarchy `Json:"Children,omitempty"`
+type Heirarchy_json struct {
+	Id            string            `json:"Id"`
+	TechnicalName string            `json:"TechnicalName"`
+	Type          string            `json:"Type"`
+	Children      *[]Heirarchy_json `Json:"Children,omitempty"`
 }
 
 type Character struct {
@@ -37,6 +29,20 @@ type Character struct {
 	Image_path string
 }
 
+type _Character_json struct {
+	Objects []struct {
+		Type       string `json:"Type"`
+		Properties struct {
+			DisplayName  string `json:"DisplayName"`
+			Image_path   string `json:"Image_path"`
+			PreviewImage struct {
+				Asset string `json:"Asset"`
+			} `json:"PreviewImage"`
+		} `json:"Properties"`
+	} `json:"Objects"`
+}
+
+// extract manifest.json and map package name to the corresponding file path
 func ExtractPackageMap(top_level_path string, filename string) (map[string]string, error) {
 
 	data, err := os.ReadFile(top_level_path + filename)
@@ -44,7 +50,7 @@ func ExtractPackageMap(top_level_path string, filename string) (map[string]strin
 		return nil, err
 	}
 
-	var manifest Manifest
+	var manifest _Manifest_json
 	if err := json.Unmarshal(data, &manifest); err != nil {
 		return nil, err
 	}
@@ -56,22 +62,27 @@ func ExtractPackageMap(top_level_path string, filename string) (map[string]strin
 	return result, nil
 }
 
-// func ExtractCharacterDefinitions(package_manifest map[string]string) (Character, error) {
-// 	data, err := os.ReadFile(package_manifest["Character_Exoorts"])
-// 	if err != nil {
-// 		return Character{}, err
-// 	}
-// 	data
-// }
+func ExtractCharacterDefinitions(package_manifest map[string]string) (Character, error) {
+	data, err := os.ReadFile(package_manifest["Character_Exoorts"])
+	if err != nil {
+		return Character{}, err
+	}
+	var _character _Character_json
+	if err := json.Unmarshal(data, &_character); err != nil {
+		return Character{}, err
+	}
+	// _character.Objects
+}
 
-func IdToTechnicalName(heirarchy_data *Heirarchy) (map[string]string, error) {
+// map Object IDs to Object TechnicalNames
+func IdToTechnicalName(heirarchy_data *Heirarchy_json) map[string]string {
 	output := make(map[string]string, 0)
-	var queue []Heirarchy
+	var queue []Heirarchy_json
 
 	output[heirarchy_data.Id] = heirarchy_data.TechnicalName
 
 	if heirarchy_data.Children == nil {
-		return output, nil
+		return output
 	}
 	queue = append(queue, *heirarchy_data.Children...)
 	for len(queue) > 0 {
@@ -85,7 +96,7 @@ func IdToTechnicalName(heirarchy_data *Heirarchy) (map[string]string, error) {
 
 	}
 
-	return output, nil
+	return output
 
 }
 func main() {
@@ -104,13 +115,13 @@ func main() {
 		return
 	}
 
-	var heirarchy_data Heirarchy
+	var heirarchy_data Heirarchy_json
 	if err := json.Unmarshal(data, &heirarchy_data); err != nil {
 		fmt.Println("error parsing JSON:", err)
 		return
 	}
 
-	id_map, _ := IdToTechnicalName(&heirarchy_data)
+	id_map := IdToTechnicalName(&heirarchy_data)
 
 	fmt.Println(len(id_map))
 
